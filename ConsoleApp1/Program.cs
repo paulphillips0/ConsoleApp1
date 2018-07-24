be053714b4af980091ae05a11e3be0b90ec07a26
Paul Phillips <stratman1234@gmail.com>
2018-07-23 23:38:57 -0700

    Revert "Checked the date and cleaned up"
    This reverts commit 24531e03c99f9602e1d550fe3b03f7c4385aa5d3.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,13 +1,20 @@
-﻿namespace ConsoleApp1
-{
-    using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ConsoleApp1
+{
     class Program
     {
         static void Main(string[] args)
         {
+<<<<<<< HEAD
             DateTime now = DateTime.Now.ToUniversalTime();
             Console.WriteLine($"Current UTC time is {now}");
+=======
+>>>>>>> parent of 24531e0... Checked the date and cleaned up
         }
     }
 }
