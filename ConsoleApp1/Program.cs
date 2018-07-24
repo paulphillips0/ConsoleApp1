841f885f58f313e387ef553863d7fe10a55bfa68
Paul Phillips <stratman1234@gmail.com>
2018-07-23 23:58:50 -0700

    Revert "Used UTC time in Main"
    This reverts commit 64a9422df0b2c644027c47f42752b2314a1ff3f2.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,6 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-
             DateTime now = DateTime.Now.ToUniversalTime();
             Console.WriteLine($"Current UTC time is {now}");
         }
