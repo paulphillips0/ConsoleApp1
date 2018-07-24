24531e03c99f9602e1d550fe3b03f7c4385aa5d3
Paul Phillips <stratman1234@gmail.com>
2018-07-23 22:50:13 -0700

    Checked the date and cleaned up

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp1
+﻿namespace ConsoleApp1
 {
+    using System;
+
     class Program
     {
         static void Main(string[] args)
         {
+            DateTime now = DateTime.Now;
         }
     }
 }
